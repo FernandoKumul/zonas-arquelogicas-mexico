c03f33480871e69bf08830cb714a057439d81b31
FernandoKumul <ferdesarrollador11@outlook.com>
2023-11-06 01:34:18 -0500

    add generate info zone

--- scripts/generate-zones.js
+++ scripts/generate-zones.js
@@ -7,5 +7,42 @@ let id = Number(url.searchParams.get("id"));
 generateItem(zones[id-1])
 
 function generateItem(zone){
-    
+    const idInfoZone = document.getElementById('info-zone');
+
+    if(zone === undefined){
+        idInfoZone.innerHTML = `<div class="error-msg">No se encontró la zona arqueológica</div>`
+        return
+    }
+
+    const name = document.getElementById('name')
+    const photo = document.getElementById('container-photo')
+    const state = document.getElementById('state')
+    const access = document.getElementById('access')
+    const meaning = document.getElementById('meaning')
+    const culture = document.getElementById('culture')
+    const coordinates = document.getElementById('coordinates')
+    const map = document.getElementById('map')
+
+    name.innerText = zone.nombre
+    photo.innerHTML = `<img class="img-zone" src=${zone.foto}>`
+    state.innerText = zone.estado
+    access.innerText = zone.abierto ? "Abierto" : "Cerrado" 
+    meaning.innerText = zone.significado
+    coordinates.innerHTML = zone.coordenadas
+    map.innerHTML = `<iframe src="${zone.map}"
+        width="600" height="450" style="border:0;" allowfullscreen="" 
+        loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+    </iframe>`
+
+    if(typeof zone.relevanciaCultural === 'string'){
+        culture.innerHTML = `<p>${zone.relevanciaCultural}</p>`
+    }else {
+        for (const subtheme in zone.relevanciaCultural) {
+            culture.innerHTML += `
+                <h5>${subtheme}</h5>
+                <p>${zone.relevanciaCultural[subtheme]}</p>
+            `
+        }
+    }
+
 }
