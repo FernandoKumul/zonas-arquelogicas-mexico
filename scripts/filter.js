30a75d4087bc26996ff532bc9dc6bd6de033c97b
FernandoKumul <ferdesarrollador11@outlook.com>
2023-11-09 02:52:21 -0500

    filters and card finished

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,6 +1,7 @@
 import zones from '../data/zonas.json' assert { type: 'json'}
 
 const refSelect = document.getElementById("states-select")
+const numZones = document.getElementById("num-zones")
 const containerZone = document.getElementById("container-zones")
 
 refSelect.addEventListener('change', generateCards)
@@ -9,10 +10,31 @@ document.addEventListener('DOMContentLoaded', generateCards);
 
 function generateCards () {
     const state = refSelect.value
+    let countZones = 0
     let cardsZones = ""
     for (const zone of zones){
-        if (zone.estado.includes(state)) cardsZones += `<li>${zone.nombre}</li>`
+        if (zone.estado.includes(state)) {
+            countZones++
+            cardsZones += `        
+            <div class="card-item">
+                <div class="content-img">
+                    <img src="${zone.foto}" alt="${zone.name}">
+                </div>
+                <p class="name">${zone.nombre}</p>
+                <div class="card-info">
+                    <div class="state-location">
+                        <img src="./assets/bxs-map.svg" alt="icon-map">
+                        <p>${zone.estado}</p>
+                    </div>
+                    <div class="chip ${zone.abierto ? "open" : "close"}">
+                        <p>${zone.abierto ? "Abierto al público" : "Cerrado al público"}</p>
+                    </div>
+                </div>
+                <a class="button-more" href="./zona.html?id=${zone.id}">Saber más...</a>
+            </div>`
+        }
     }
 
     containerZone.innerHTML = cardsZones
+    numZones.innerText = countZones
 }
